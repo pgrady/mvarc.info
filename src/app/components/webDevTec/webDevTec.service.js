6fb7f94ec2e90f6812e37163b3803f353c35ea77
Patrick Grady <patrick.grady@eglobaltech.com>
2016-02-22 12:07:41 -0500

    Eric's suggestions for promises and proper consumption.

--- src/app/components/webDevTec/webDevTec.service.js
+++ src/app/components/webDevTec/webDevTec.service.js
@@ -6,8 +6,12 @@
       .service('webDevTec', webDevTec);
 
   /** @ngInject */
-  function webDevTec($log, $rootScope, $q) {
-   // AWS.config.region = 'us-east-1'; // Region
+  function webDevTec($log, $q) {
+
+    AWS.config.region = 'us-east-1'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'us-east-1:14f52e55-aad9-4992-b555-72185761caaf'
+    });
 
     var s3 = new AWS.S3({apiVersion: '2006-03-01', sslEnabled: false});
 
@@ -27,55 +31,10 @@
 
     }
 
-
     return {
       loadMVARC: loadMVARC
     };
 
-    var data = [
-
-      {
-        "title": "Testing March 12, 2016",
-        "url": "https://www.google.com/maps/place/First+Christian+Church+of+Alexandria/",
-        "url_label": "Location on Google Maps",
-        "description": "Testing starts at 9:30 am at First Christian Church, King Street, Alexandria",
-        "logo": "LarcVEC175.png",
-        "rank": 1
-      }, {
-        "title": "Testing April 9, 2016",
-        "url": "https://www.google.com/maps/place/First+Christian+Church+of+Alexandria/",
-        "url_label": "Location on Google Maps",
-        "description": "Testing starts at 9:30 am at First Christian Church, King Street, Alexandria",
-        "logo": "LarcVEC175.png",
-        "rank": 2
-      }, {
-        "title": "Mount Vernon ARC",
-        "url": "http://mvarc.org",
-        "url_label": "Club Web Site",
-        "description": "In and around George Washington's Mount Vernon Estate",
-        "logo": "radio-icon.png",
-        "rank": 3
-      }
-    ];
-    AWS.config.region = 'us-east-1'; // Region
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: 'us-east-1:14f52e55-aad9-4992-b555-72185761caaf'
-    });
-
-    this.getTec = getTec;
-
-    function getTec() {
-      var s3 = new AWS.S3({apiVersion: '2006-03-01', sslEnabled: false});
-      s3.getObject({Bucket:'mvarc.info.data',Key:'data.json'}, function(err, data){
-        if (err) {
-          $log.error(err, err.stack);
-        } else {
-          $log.info(data.Body.toString());
-        }
-      });
-
-      return data;
-    }
   }
 
-})();
+});
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -17,13 +17,13 @@
     vm.mvarc = null;
 
     webDevTec.loadMVARC().then(function(data) {
-      mv.mvarc = data;
+      vm.mvarc = data;
     });
 
     activate();
 
     function activate() {
-      getWebDevTec();
+      webDevTec.loadMVARC();
       $timeout(function() {
         vm.classAnimation = 'rubberBand';
       }, 4000);
@@ -34,12 +34,12 @@
       vm.classAnimation = '';
     }
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
+  //  function getWebDevTec() {
+  //    vm.awesomeThings = webDevTec.getTec();
 
 //      angular.forEach(vm.awesomeThings, function(awesomeThing) {
 //        awesomeThing.rank = Math.random();
-//      });
-    }
+  //      });
+//    }
   }
 })();
